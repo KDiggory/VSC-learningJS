7437319f31e1d196d7eaee14313e62607505c86a
Katie Diggory <Katie.diggory@gmail.com>
2021-12-06 19:09:55 +0000

    Day 5, learning bootstrap and cont resreq

--- JavaScript/JSDay5/bootstrap.js
+++ JavaScript/JSDay5/bootstrap.js
@@ -1,3 +1,13 @@
 "use strict";
 
 // bootstrap //
+// bootstrap is a CSS framework
+// prewritten styling
+// apply classes to your elements
+// bootstrapping is used for the initial configuration of an application - it helps you get off the ground
+// add in with this link
+// <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+// how does bootstrap style forms?
+// put bootstrap in before you import your css file, also you can use classes to be more specific!
+
